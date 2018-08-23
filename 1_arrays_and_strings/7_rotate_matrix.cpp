888f4712e3ee6ac56448355cb4b8c5d91235bff9
mbiggio <matteobiggio85@libero.it>
2018-08-23 22:52:20 +0200

    arrays and strings: 7 - rotate matrix

---
+++ 1_arrays_and_strings/7_rotate_matrix.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+using namespace std;
+
+
+/*
+ * Algorithm to rotate an NxN 2D matrix by 90 degrees clockwise.
+ */
+
+void rotate_matrix(vector<vector<int>> &matrix) {
+  int kSize = matrix.size();
+  for (int s=0; s<kSize/2; ++s) {
+    for (int i=0; s+i<kSize-1-s; ++i) {
+      int cur = matrix[s][s+i];
+      swap(cur,matrix[s+i][kSize-1-s]);
+      swap(cur,matrix[kSize-1-s][kSize-1-s-i]);
+      swap(cur,matrix[kSize-1-s-i][s]);
+      swap(cur,matrix[s][s+i]);
+    }
+  }
+}
+
+void print(const vector<vector<int>> &m) {
+  for (const auto &r : m) {
+    for (const auto &c : r) {
+      printf("%2d ", c);
+    }
+    cout << "\n";
+  }
+  cout << "\n";
+}
+
+int main() {
+  {
+    vector<vector<int>> m = {{ 1, 2, 3, 4},
+                             { 5, 6, 7, 8},
+                             { 9,10,11,12},
+                             {13,14,15,16}};
+    print(m);
+    rotate_matrix(m);
+    print(m);
+  }
+  {
+    vector<vector<int>> m = {{ 1, 2, 3},
+                             { 4, 5, 6},
+                             { 7, 8, 9}};
+    print(m);
+    rotate_matrix(m);
+    print(m);
+  }
+  return 0;
+}
