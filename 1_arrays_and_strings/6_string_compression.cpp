9a604d0c41f0b7e01fd282e53bd22d9dbea18c5c
Matteo Biggio <matteobiggio85@libero.it>
2018-08-23 17:17:43 +0200

    1.6: string_compression

---
+++ 1_arrays_and_strings/6_string_compression.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+/*
+ * Compress a string by grouping together repeated characters.
+ * If the resulting string after compression is bigger than the original,
+ * return the original.
+ */
+
+string compress_string(const string &s) {
+  stringstream result;
+  for (int i=0; i<s.size(); ++i) {
+    int count = 0;
+    char cur = s[i];
+    while(i<s.size() && s[i]==cur) {
+      ++count;
+      ++i;
+    }
+    --i;
+
+    result << cur << count;
+
+  }
+  return result.str().size()<s.size()?result.str():s;
+}
+
+int main() {
+  cout << compress_string("aabcccccaaa") << endl;
+  cout << compress_string("abca") << endl;
+  return 0;
+}
